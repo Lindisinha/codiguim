50692207f14dcfeacf5a5987ea8175c290948419
Lindisinha <136626682+Lindisinha@users.noreply.github.com>
2023-08-18 10:06:26 -0400

    Add files via upload
    atividade de herança poo

---
+++ Conta.cs
@@ -0,0 +1,20 @@
+﻿public class Conta
+{
+    public int nConta;
+    public string agencia;
+    public string titular;
+    public double saldo;
+
+    public void Saque(double valor)
+    {
+        if ( valor <= this.saldo ) 
+        {
+            this.saldo = saldo - valor;
+        }
+    }
+    public void Deposito(double deposito)
+    {
+        this.saldo = saldo + deposito;
+    }
+}
+
---
+++ ContaEstudante.cs
@@ -0,0 +1,20 @@
+﻿public class ContaEstudante : Conta
+{
+    public double chequeEsp= 2000;
+    public string cpf;
+    public string instituicao;
+
+    public virtual void Saque(double valor)
+    {
+        if (valor <= this.saldo + chequeEsp)
+        {
+            this.saldo = saldo - valor;
+            if ( this.saldo < 0 ) 
+            {
+                this.chequeEsp = this.chequeEsp + saldo;
+                this.saldo = 0;
+            }
+        }
+    }
+
+}
