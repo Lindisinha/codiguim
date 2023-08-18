b6f5e8839b1cee6f33fb9f7b7e2185ef685ced95
Lindisinha <136626682+Lindisinha@users.noreply.github.com>
2023-08-18 14:45:54 -0400

    Add files via upload
    herança2tst

---
+++ herança/ContaEmpresario.cs
@@ -0,0 +1,34 @@
+﻿public class ContaEmpresario : Conta
+{
+    public double anuidade;
+    public double limiteEmprestimo = 10000;
+    public double totalEmprestimo;
+
+    public void Emprestimo(double valor)
+    {
+        if (valor <= limiteEmprestimo)
+        {
+            this.saldo = saldo + valor;
+            this.totalEmprestimo += valor;
+            Console.WriteLine("Empréstimpo realizado com sucesso");
+        }
+        else
+        {
+            Console.WriteLine("O valor de empréstimo não pode ser concedido");
+        }
+    }
+    public virtual void Saque(double valor)
+    {
+        if (valor <= this.saldo)
+        {
+            if (valor >= 5000)
+            {
+                this.saldo = saldo - (valor + 5);
+            }
+            else
+            {
+                this.saldo = saldo - valor;
+            }
+        }
+    }
+}
---
+++ herança/ContaEstudante.cs
@@ -0,0 +1,15 @@
+﻿public class ContaEstudante : Conta
+{
+    public double chequeEsp = 2000;
+    public string cpf;
+    public string instituicao;
+
+    public virtual void Saque(double valor)
+    {
+        if (valor <= this.saldo + chequeEsp)
+        {
+            this.saldo -= valor;
+        }
+    }
+
+}
